e5becde8bab728773e3100c17e62608aae86ffcd
ming42889 <ming42889@hotmail.com>
2014-08-25 22:54:31 -0400

    1

--- cavalier/src/lectureEx/PrintDir.java
+++ cavalier/src/lectureEx/PrintDir.java
@@ -6,13 +6,20 @@ import java.util.Stack;
 public class PrintDir {
 	public static void main(String[] args){
 		File file = new File(new File("").getAbsolutePath());	//current dir
-		String root = file.toPath().getRoot().toString();				//root dir
+		String root = file.toPath().getRoot().toString();//root dir
 		Stack<String> s = new Stack<String>();
 		
 		System.out.println(root);
+		System.out.println(new File("2").getAbsolutePath());
 		for (File f:file.listFiles()){
 			if (f.getName()=="Users")
 			System.out.print(" ->");
+		}
+		
+		
+		String ss = "";
+		for(;file!=null;file=file.getParentFile()){
+			System.out.println(file.getName());
 		}
 		
 		
